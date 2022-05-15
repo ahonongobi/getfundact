0dbc9684f96d9d7b9f1ee6068db97348bed42eba
ahonongobi <abyssiniea@gmail.com>
2022-05-15 15:50:32 +0200

    solde 1

--- resources/views/user_dash/index.blade.php
+++ resources/views/user_dash/index.blade.php
@@ -67,15 +67,15 @@
             @foreach ($all_campagnes as $item)
             <div class="col-sm-6 col-lg-4">
                 <div class="donation-item">
-                    <div  class="img">
+                    <div class="img">
                         <img style="height: 400px !important" src="{{asset('storage/UserDocument/'.$item->file_vignette)}}" alt="Donation">
-                        <a class="common-btn" href="{{ url('getfund-donation-details/'.$item->id.'/'.$item->name_b) }}">Contribuer</a>
+                        <a class="common-btn" href="{{ url('donation-details/'.$item->id.'/'.$item->name_b) }}">Contribuer</a>
                     </div>
                     <div class="inner">
                         <div class="top">
                             <a class="tags" href="#">#{{$item->categories}}</a>
                             <h3>
-                                <a href="{{ url('getfund-donation-details/'.$item->id.'/'.$item->name_b) }}">{{$item->name}}</a>
+                                <a href="{{ url('donation-details/'.$item->id.'/'.$item->name_b) }}">{{$item->name}}</a>
                             </h3>
                             <p>
                                 @php
@@ -92,86 +92,88 @@
                                 @endphp
                                 <div  class="progress-bar  progress-bar-striped bg-success progress-bar-animated" role="progressbar" style="width: {{ $xpercent }}%; background-color: #ff6015 !important" aria-valuenow="{{$xpercent}}" aria-valuemin="0" aria-valuemax="100">{{$x}}%</div>
                               </div>
-                             {{--@if ($item->montant_cotise==0.00)
-                             <div class="skill">
-                                <div class="skill-bar skill0 wow fadeInLeftBig">
+                             
+                            {{--@if ($item->montant_cotise==0)
+                            <div class="skill">
+                               <div class="skill-bar skill0 wow fadeInLeftBig">
 
-                                    
-                                    <span class="skill-count0">0%</span>
-                                </div>
-                            </div>
-                             @endif
+                                   
+                                   <span class="skill-count0">0%</span>
+                               </div>
+                           </div>
+                            @endif
+                           
+                           @if ($item->montant_cotise==(1/10)*$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill10 wow fadeInLeftBig">
+
+                                   
+                                   <span class="skill-count10">10%</span>
+                               </div>
+                           </div>
+                           @endif
+                           
+                           @if ($item->montant_cotise==(1/20)*$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill20 wow fadeInLeftBig">
+
+                                   
+                                   <span class="skill-count20">20%</span>
+                               </div>
+                           </div>
+                           @endif
+                           @if ($item->montant_cotise==(1/50)*$item->montant_v)
+                           <div class="skill">
+                              
+                               <div class="skill-bar skill50 wow fadeInLeftBig">
+
+                                   <span class="skill-count50">50%</span>
+                               </div>
+                           </div>     
+                           @endif
+                           
+                           @if ($item->montant_cotise==(1/75)*$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill75 wow fadeInLeftBig">
+
+                                   
+                                   <span class="skill-count75">75%</span>
+                               </div>
+                           </div>
+                           @endif
+                           @if ($item->montant_cotise==(1/85)*$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill1 wow fadeInLeftBig">
+
+                                   <span class="skill-count1">85%</span>
+                                   
+                               </div>
+                           </div>
+                           @endif
+                           @if ($item->montant_cotise==(1/95)*$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill95 wow fadeInLeftBig">
+
+                                   <span class="skill-count95">95%</span>
+                                   
+                               </div>
+                           </div>
+                           @endif
+                           @if ($item->montant_cotise==$item->montant_v)
+                           <div class="skill">
+                               <div class="skill-bar skill100 wow fadeInLeftBig">
+
+                                   <span class="skill-count100">100%</span>
+                                   
+                               </div>
+                           </div>
+                           @endif
+                           --}}
+
                             
-                            @if (0 < $item->montant_cotise AND $item->montant_cotise < (10*$item->montant_v)/100)
-                            <div class="skill">
-                                <div class="skill-bar skill10 wow fadeInLeftBig">
-
-                                    
-                                    <span class="skill-count10">10%</span>
-                                </div>
-                            </div>
-                            @endif
-                            
-                            @if ($item->montant_cotise==(1/20)*$item->montant_v)
-                            <div class="skill">
-                                <div class="skill-bar skill20 wow fadeInLeftBig">
-
-                                    
-                                    <span class="skill-count20">20%</span>
-                                </div>
-                            </div>
-                            @endif
-                            @if ($item->montant_cotise==(1/50)*$item->montant_v)
-                            <div class="skill">
-                               
-                                <div class="skill-bar skill50 wow fadeInLeftBig">
-
-                                    <span class="skill-count50">50%</span>
-                                </div>
-                            </div>     
-                            @endif
-                            
-                            @if ($item->montant_cotise==(1/75)*$item->montant_v)
-                            <div class="skill">
-                                <div class="skill-bar skill75 wow fadeInLeftBig">
-
-                                    
-                                    <span class="skill-count75">75%</span>
-                                </div>
-                            </div>
-                            @endif
-                            @if ($item->montant_cotise==(1/85)*$item->montant_v)
-                            <div class="skill">
-                                <div class="skill-bar skill1 wow fadeInLeftBig">
-
-                                    <span class="skill-count1">85%</span>
-                                    
-                                </div>
-                            </div>
-                            @endif
-                            @if ($item->montant_cotise==(1/95)*$item->montant_v)
-                            <div class="skill">
-                                <div class="skill-bar skill95 wow fadeInLeftBig">
-
-                                    <span class="skill-count95">95%</span>
-                                    
-                                </div>
-                            </div>
-                            @endif
-                            @if ($item->montant_cotise==$item->montant_v)
-                            <div class="skill">
-                                <div class="skill-bar skill100 wow fadeInLeftBig">
-
-                                    <span class="skill-count100">100%</span>
-                                    
-                                </div>
-                            </div>
-                            @endif
-
-                            --}}
                             <ul>
-                                   <li>Montant contribué:  @php
-                                    if($item->montant_cotise==0)
+                                <li>Montant contribué:  @php
+                                    if($item->montant_cotise==NULL)
                                     echo "0 FCFA";
 
                                     else {
@@ -180,9 +182,9 @@
                                 @endphp</li>
                                 <li>Montant visé: {{$item->montant_v}} FCFA</li>
                             </ul>
-                           {{--<h4>Contributions: 
+                            <!--<h4>Contributions: 
                                 <span>60 personnes</span>
-                            </h4>--}}
+                            -->
                         </div>
                     </div>
                 </div>
