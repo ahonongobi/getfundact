543d35394a3357db85a237ceffdca133ffc482d8
ahonongobi <abyssiniea@gmail.com>
2022-06-10 15:23:39 +0200

    facebook add

--- resources/views/_layouts/_head.blade.php
+++ resources/views/_layouts/_head.blade.php
@@ -52,6 +52,25 @@
             
 
         </style>
+        <!-- Facebook Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2294101097407796'); 
+            fbq('track', 'PageView');
+            </script>
+            <noscript>
+            <img height="1" width="1" 
+            src="https://www.facebook.com/tr?id=2294101097407796&ev=PageView
+            &noscript=1"/>
+            </noscript>
+    <!-- End Facebook Pixel Code -->
     </head>
     <body style="font-family: montserrat; ">
 
@@ -268,7 +287,7 @@
                     <div class="footer-links">
                         <h3>Liens utils</h3>
                         <ul>
-                            <li><a href="/all-campagnes"><i class="icofont-simple-right"></i>Exemples de cagnotte</a></li>
+                            <li><a href="/all-campagnes"><i class="icofont-simple-right"></i>Exemples de campagne</a></li>
                             <li><a href="/faq"><i class="icofont-simple-right"></i>FAQ/Aide</a></li>
                             <li><a href="/#"><i class="icofont-simple-right"></i>Evenements</a></li>
                             <li><a href="/tarifs"><i class="icofont-simple-right"></i>Tarifs</a></li>
